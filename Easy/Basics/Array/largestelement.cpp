b530eb0f8c41ad3caebcff0b1868a5cc16c1f1d9
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-04-15 23:58:02 +0530

    15/04/24

---
+++ Easy/Basics/Array/largestelement.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+// Brute Force
+int bruteMeth(vector<int> arr,int n){
+    sort(arr.begin(),arr.end());
+    return arr[n-1];
+}
+// Time Complexity: O(nlogn)
+
+// Optimal Approach
+int optimalMeth(vector<int> arr,int n){
+   int largest = arr[0];
+   for(int i = 1;i<n;i++){
+       if(arr[i] > largest){
+           largest = arr[i];
+       }
+   }
+   return largest;
+}
+// Time Complexity: O(n)
+
+int largestElement(vector<int> &arr, int n) {
+    int brute = bruteMeth(arr,n);
+    int optimal = optimalMeth(arr,n);
+    return optimal;
+}
