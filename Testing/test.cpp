732406ab391cc33e1a64157374832a10decb3d59
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-06-18 01:43:05 +0530

    18/06/24

--- Testing/test.cpp
+++ Testing/test.cpp
@@ -1,89 +1,26 @@
-//{ Driver Code Starts
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+int brute(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int len = 0,maxi = 0;
 
-// } Driver Code Ends
-class Solution{
-    private:
-    int brute(int arr[],int n,int target){
-       int maxi = 0;
-       for(int i = 0; i < n; i++){
-           for(int j = i; j < n;j++){
-               int sum = 0;
-               for(int k = i;k <= j;k++){
-                   sum+=arr[k];
-                   if(sum == target){
-                       maxi = max(maxi,j - i + 1);
-                   }
-               }
-               
-           }
-           
-       }
-       return maxi;
-    }
-    
-    int better_One(int arr[],int n,int target){
-       int maxi = 0;
-       for(int i = 0; i < n; i++){
-            int sum = 0;
-           for(int j = i; j < n;j++){
-               sum += arr[j];
-               if(sum == target){
-               maxi = max(maxi,j - i + 1);
+        for(int i = 1 ; i < nums.size() ; i++){
+            if(nums[i - 1] + 1 == nums[i]){
+                cout<<nums[i - 1] + 1 << " == " <<nums[i]<<endl;
+                len++;
+                
+                cout<<"Maxi In:"<<maxi<<endl;
+                cout<<"Len: "<<len<<endl;
+            }else{
+                len = 0;
             }
-            }
-            
-       }
-       return maxi;
+        }
+        cout<<"Maxi: "<<maxi<<endl;
+        return maxi + 1;
     }
-    
-    int better_Two(int arr[],int n,int target){
-       int maxi = 0;
-       map<int,int> mpp;
-       int sum = 0;
-       for(int i = 0; i < n; i++){
-           sum+=arr[i];
-           int remaining = target - sum;
-           if(mpp.find(remaining) != mpp.end()){
-               int len = i - mpp[remaining];
-               maxi = max(maxi,len);
-           }
-           else if(mpp.find(sum) == mpp.end()){
-               mpp[sum] = i;
-           }
-       }
-       return maxi;
-    }
-    
-    public:
-    int lenOfLongSubarr(int A[],  int N, int K) 
-    { 
-       return better_Two(A,N,K);
-    } 
 
-};
-
-//{ Driver Code Starts.
-
-int main() {
-	//code
-	
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n, k;
-	    cin>> n >> k;
-	    int a[n];
-	    
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	   Solution ob;
-	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
-	    
-	}
-	
-	return 0;
+int main(){
+    vector<int> arr = {0,3,7,2,5,8,4,6,0,1};
+    return brute(arr);
 }
-// } Driver Code Ends
