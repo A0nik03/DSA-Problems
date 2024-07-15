d806832f8c229efb25b1cb3ce63baae696f6034a
Bhanu Pratap Singh <nikrcnkhil@gmail.com>
2024-07-15 08:16:08 +0530

    15/01/24

---
+++ Medium/Binary Search Tree/floor_in_a_BST.cpp
@@ -0,0 +1,90 @@
+// Floor in BST
+
+
+/*
+You are given a BST(Binary Search Tree) with n number of nodes and value x. your task is to find the greatest value node of the BST which is smaller than or equal to x.
+Note: when x is smaller than the smallest node of BST then returns -1.
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    Node *right;
+    Node *left;
+
+    Node(int x) {
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// Function to search a node in BST.
+class Solution{
+
+public:
+    int floor(Node* root, int x) {
+        if(root == NULL) return -1;
+        
+        int floor = -1;
+        while(root){
+            if(root->data == x){
+                floor = root->data;
+                return floor;
+            }
+            
+            if(root->data > x){
+                root = root->left;
+            }
+            else{
+                floor = root->data;
+                root = root->right;
+            }
+        }
+        return floor;
+    }
+};
+
+//{ Driver Code Starts.
+
+Node *insert(Node *tree, int val) {
+    Node *temp = NULL;
+    if (tree == NULL) return new Node(val);
+
+    if (val < tree->data) {
+        tree->left = insert(tree->left, val);
+    } else if (val > tree->data) {
+        tree->right = insert(tree->right, val);
+    }
+
+    return tree;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        Node *root = NULL;
+
+        int N;
+        cin >> N;
+        for (int i = 0; i < N; i++) {
+            int k;
+            cin >> k;
+            root = insert(root, k);
+        }
+
+        int s;
+        cin >> s;
+        Solution obj;
+        cout << obj.floor(root, s) << "\n";
+    }
+}
+
+// } Driver Code Ends
